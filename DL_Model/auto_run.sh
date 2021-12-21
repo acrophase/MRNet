5f82d20a025ea7e234228da8e8a87b4e046a8256
Kapil19-dev <rsingh2@student.nitw.ac.in>
2021-12-21 11:37:54 +0530

    default argument added

--- DL_Model/auto_run.sh
+++ DL_Model/auto_run.sh
@@ -4,11 +4,17 @@ TESTBENCH_PATH='/media/acrophase/pose1/Kapil/MultiRespDL/DL_Model/new_testbench.
 Data_Path='/media/acrophase/pose1/Kapil/ppg_dalia_data'
 Saved_Model_Path='/media/acrophase/pose1/Kapil/MultiRespDL/DL_Model/SAVED_MODELS'
 Annotation_Path='/media/acrophase/pose1/Kapil/MultiRespDL/DL_Model/annotation.pkl'
+
+srate=700
+window_len=32
+number_epoch=1
+ID=13
+
 echo "---------------------File Generation Begins ----------------"
 ############################# DATA GENERATOR ###############################
-echo "python ${FILE_GEN_PATH} --data_path "Data_Path""
-python ${FILE_GEN_PATH} --data_path ${Data_Path}
+echo "python ${FILE_GEN_PATH} --data_path "Data_Path --srate ${srate} --win_len ${window_len}""
+python ${FILE_GEN_PATH} --data_path ${Data_Path} --srate ${srate} --win_len ${window_len}
 ############################# NEW TESTBENCH BEGINS ###############################
 echo "python ${TESTBENCH_PATH} --save_model_path "Saved_Model_Path""
-python ${TESTBENCH_PATH} --save_model_path ${Saved_Model_Path} --annot_path ${Annotation_Path}
+python ${TESTBENCH_PATH} --save_model_path ${Saved_Model_Path} --srate ${srate} --win_len ${window_len} --num_epochs ${number_epoch} --train_test_split_id ${ID} --annot_path ${Annotation_Path}
 echo "---------------------Model Training Complete ----------------"
